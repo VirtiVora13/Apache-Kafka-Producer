76421ddcd5dd79bda7c9784386418a72d9c8177e
VirtiVora13 <97144928+VirtiVora13@users.noreply.github.com>
2022-08-24 17:09:18 +0530

    Add files via upload
    add project implementing Apache-Kafka-Producer

---
+++ src/main/java/com/example/kafka/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.kafka;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Autowired
+    KafkaTemplate<String,String> kafkaTemplate;
+
+    private static final String TOPIC = "NewTopic";
+
+    @GetMapping("/publish/{message}")
+    public String publishMessage(@PathVariable("message") final String message)
+    {
+        kafkaTemplate.send(TOPIC, message);
+        return "Message published successfully";
+    }
+
+}
